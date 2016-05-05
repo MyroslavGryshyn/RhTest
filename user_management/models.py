90bdddb1e1eb44a1a59353eab879bffe77df442c
mero <sergiy.1911@gmail.com>
2016-05-05 12:13:16 +0300

    Created model for storing users

--- user_management/models.py
+++ user_management/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """
+    Model for storing users.
+    """
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    iban = models.IntegerField()
