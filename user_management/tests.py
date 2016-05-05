c7adbb80f0f660e19f6586213c064413c1b7a1e5
mero <sergiy.1911@gmail.com>
2016-05-05 14:45:44 +0300

    Added test of UserProfileCreateView

--- user_management/tests.py
+++ user_management/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import UserProfile
 
-# Create your tests here.
+
+class UserProfileCreateTest(TestCase):
+
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/add/',
+            data={'first_name': "John",
+                  'last_name': "Doe",
+                  'iban': "111111111"}
+        )
+
+        self.assertEqual(UserProfile.objects.count(), 1)
+        new_user_profile = UserProfile.objects.first()
+        self.assertEqual(new_user_profile.iban, 111111111)
