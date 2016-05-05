a564c430fe4d4decfa2999b0bc2c31d553d8cb4e
mero <sergiy.1911@gmail.com>
2016-05-05 17:58:15 +0300

    Finished renaming user to customer

--- RhTest/urls.py
+++ RhTest/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from user_management.views import (
+from customer_management.views import (
     CustomerCreateView, CustomerListView,
     CustomerUpdateView, CustomerDeleteView
 )
